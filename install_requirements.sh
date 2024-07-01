5ca991ee25474fe0ee5f606cc340d4376ce6afc3
Dmitrii Victorovich Shaulskii <shaulskiid@gmail.com>
2024-07-01 11:58:55 +0200

    req

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,7 +2,11 @@ pip install -r requirements.txt
 pip install -r requirements_after.txt
 #pip install -U mmcv
 #mim install mmcv
-pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cpu/torch2.1/index.html  # for cpu MacOS
-#pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu121/torch2.1/index.html  # for gpu Linux
+if [ "$(uname -s)" == "Linux" ]; then
+    pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu102/torch1.7.0/index.html
+else
+    pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.7.0/index.html
+fi
+
 
 
